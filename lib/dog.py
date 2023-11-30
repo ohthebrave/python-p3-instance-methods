5fc9e57f5948546cfd86f5ae149af6e14a10118c
George <georgejoj28@gmail.com>
2023-11-30 11:17:24 +0300

    completed

--- lib/dog.py
+++ lib/dog.py
@@ -4,4 +4,18 @@ class Dog:
     # Class body goes here
 
     #Instance method definition
-    pass
+    # bark becomes a method of all instances of Dogs.
+    def bark(self):
+        print('whoof!')
+
+    # sit becomes a method 
+    def sit(self):
+        print("The dog is sitting.")
+
+
+fido = Dog()
+fido.bark()
+# output =>> Woof!
+
+snoopy = Dog()
+snoopy.sit()
